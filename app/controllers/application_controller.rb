8d74d926109813a9ff4cc104b141739f55b75ab0
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-18 21:35:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-  get '/' do
-    erb :'super_hero'
-  end
+  get '/' do 
+        erb :super_hero
+    end 
 
-   post '/teams' do
+    post '/teams' do
         @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
         members = params[:team][:members]
 
